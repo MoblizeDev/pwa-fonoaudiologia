f9c4b23a863d7a1f4b9f2e6a3ef18c96c35bfaae
Pablo Herzberg <37887593+pabloherzberg@users.noreply.github.com>
2020-12-10 16:21:33 -0300

    Update index.js

--- src/pages/Mobile/Contatos/index.js
+++ src/pages/Mobile/Contatos/index.js
@@ -13,22 +13,8 @@ function Contatos() {
 
     const contatos =[
   
-        {nome:'Dra. Ana Clara',telefone:5571991328666},
-        {nome:'Dra. Carolina', telefone:5571981388100},
-        {nome:'Dr. Felipe',telefone:5571999364584},
-        {nome:'Dra. Melina', telefone:5571988229570},
-        {nome:'Dra. Lílian',telefone:5586981544141},
-        {nome:'Dra. Thais', telefone:5571988803092},
-        {nome:'Dra. Sheila',telefone:5571991414862},
-        {nome:'Dra. Carla', telefone:5571991389014},
-        {nome:'Dra. Marcela',telefone:5571991583652},
-        {nome:'Dra. Joana', telefone:5571999291904},
-        {nome:'Dra. Stephanie',telefone:5571991970088},
-        {nome:'Dra. Fabiana', telefone:5571981148890},
-        {nome:'Dra. Alina', telefone:5571991829541},
-        {nome:'Dr. Hélio',telefone:5571986855922},
-        {nome:'Dr. André', telefone:5571994065155},
-        {nome:'Dr. José Rodolfo',telefone:5573999448728},
+        {nome:'Dra. Ana Clara',telefone:53999999999},
+        {nome:'Dra. Carolina', telefone:53999999999},
     ]
 
     const [msg, setMsg]= useState('Oi!')
